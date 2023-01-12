17d522d9aa61505e2b1b4ad91ee3606998d8fa6c
jorgegarciaotero <jorgegarciaotero@gmail.com>
2023-01-12 20:47:42 +0000

    v3. Changes in run.py

--- app/run.py
+++ app/run.py
@@ -8,6 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
+from plotly.graph_objs import Scatter 
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
@@ -17,36 +18,35 @@ app = Flask(__name__)
 def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
-
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-
     return clean_tokens
+
 
 # load data
 engine = create_engine('sqlite://///home/workspace/data/DisasterResponse.db')
 df = pd.read_sql_table('new_table', engine)
-print(df)
 # load model
 model = joblib.load("/home/workspace/models/classifier.pkl")
+print(df.dtypes)
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+    print("genres: ",genre_names)
+    print("genre_counts",genre_counts)
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
+    graph1 = {
             'data': [
                 Bar(
                     x=genre_names,
@@ -55,22 +55,82 @@ def index():
             ],
 
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Distribution of categories found per message',
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
-                    'title': "Genre"
+                    'title': "Count"
                 }
             }
         }
-    ]
     
-    # encode plotly graphs in JSON
+    #Top occurences for every column
+    count_ones = lambda x: (x == 1).sum()
+    ones = df[['storm','fire','earthquake','water','other_weather','floods'  ]].apply(count_ones).sort_values()
+    sorted_ones = ones.sort_values()
+    data = [Bar(
+        x=sorted_ones.index,
+        y=sorted_ones.values
+    )]
+    layout={
+                'title': 'Distribution of messages by natural climate issue',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "climate issue"
+                }
+	}
+
+    graph2={
+            'data':data,
+	    'layout':layout
+        }
+
+    #scatter
+    other_infrastructure=df.groupby('other_infrastructure').count()['message']
+    buildings=df.groupby('buildings').count()['message']
+    hospitals=df.groupby('hospitals').count()['message']
+    aid_centers=df.groupby('aid_centers').count()['message']
+    shops=df.groupby('shops').count()['message']
+    transport=df.groupby('transport').count()['message']
+    shelter=df.groupby('shelter').count()['message']
+    my_list=[hospitals[1],other_infrastructure[1],buildings[1],aid_centers[1],shops[1],transport[1],shelter[1]]
+    
+    bubble_size=list(map(lambda x: x/25, my_list)) 
+    
+    data=[Scatter(
+        x=my_list,#[0,10,20,30,40,50,60,70],
+	y=my_list,#[0,10,20,30,40,50,60,70],
+	text=['hospitals','other infra','buildings','aid_centers','shops','transport','shelter'],
+	marker=dict(
+            color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  
+		  'rgb(44, 160, 101)', 'rgb(255, 65, 54)',
+                  'rgb(153, 140, 201)', 'rgb(124, 260, 301)', 
+                  'rgb(411, 170, 11)'], 
+            size=bubble_size
+	)
+    )]
+   
+    layout={
+                'title': 'Distribution of messages by climate issue (buildings and vehicles)',
+                'yaxis': {
+                    'title': "-"
+                },
+                'xaxis': {
+                    'title': "Count"
+                }
+        }
+ 
+    graph3={
+         'data':data,
+	 'layout':layout
+    } 
+    
+    graphs=[graph1,graph2,graph3]
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
-    # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
@@ -79,11 +139,9 @@ def index():
 def go():
     # save user input in query
     query = request.args.get('query', '') 
-
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
-
     # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
